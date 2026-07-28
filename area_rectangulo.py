9baf57ea33e0243a9e89827796b49d68f9bfc338
TheDogini <ikerabdiel100@gmail.com>
2026-07-27 19:26:12 -0600

    Código simple para el cálculo de un rectángulo. Funciona incluso con valores decimales

---
+++ area_rectangulo.py
@@ -0,0 +1,4 @@
+base = float(input("¿Cuánto mide la base?: ")) #Obtiene el valor de la base
+altura = float(input("¿Cuánto mide la altura?: ")) #Obtiene el valor de la altura
+area = base * altura #Calcula el área de la figura
+print("El área del rectángulo es:", area) #Imprime el resltado
