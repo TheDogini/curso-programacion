63b055b3d2c196cc329d248f602e8e691aeb5326
TheDogini <ikerabdiel100@gmail.com>
2026-07-27 19:31:09 -0600

    Código para calcular el promedio de tres números. Funciona incluso con decimales

---
+++ promedio.py
@@ -0,0 +1,6 @@
+num1 = float(input("¿Cuál es el primer número?: ")) #Pide el primer número
+num2 = float(input("¿Cuál es el segundo número?: ")) #Pide el segundo número
+num3 = float(input("¿Cuál es el tercer número?: ")) #Pide el tercer número
+suma = num1 + num2 + num3 #Suma los tres valores
+promedio = suma / 3 #Obtiene el promedio
+print("El promedio de los números es:", promedio) #Imprime el resusltado
